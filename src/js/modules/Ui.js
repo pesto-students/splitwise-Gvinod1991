c9e347c6f206502c975fce7b5005bde3147f91e0
Godti Vinod <venkat.godti3@gmail.com>
2020-11-18 16:52:00 +0530

    Add showalert menthod third param as delay

--- src/js/modules/Ui.js
+++ src/js/modules/Ui.js
@@ -1,12 +1,12 @@
 // UI class : Handle UI tasks
 class UI {
-  static showAlert(selector, message) {
+  static showAlert(selector, message, delay = 3000) {
     document.querySelector(`#${selector}`).style.display = 'block';
     document.querySelector(`#${selector}`).innerText = message;
     setTimeout(() => {
       document.querySelector(`#${selector}`).style.display = 'none';
       document.querySelector(`#${selector}`).innerText = '';
-    }, 3000);
+    }, delay);
   }
 }
 
